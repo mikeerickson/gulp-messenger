874bc4371acf20bf4f4eac49567d2bc1ad8c6e59
Mike Erickson <codedungeon@gmail.com>
2015-05-23 13:42:08 -0700

    refactored messaging when object supplied

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ var VALUE_REGEXP = /<%=\s*([^\s]+)\s*%>/g;
 // TODO: Add support for passing options
 
 var defOptions = {
-    logToFile:     false,
-    logToConsole:  true,
-    logPath:       'logs/',
-    logFile:       'app.log',
-    timestamp:     false,
-    rotateLog:     false,
-    boldVariables: true
+	logToFile:     false,
+	logToConsole:  true,
+	logPath:       'logs/',
+	logFile:       'app.log',
+	timestamp:     false,
+	rotateLog:     false,
+	boldVariables: true
 };
 
 // SETUP WINSTON
@@ -38,235 +38,240 @@ var logger = new (winston.Logger)({ level: 'debug'});
 
 
 function notify(style, before, message, after, data) {
-    var text, variable;
-    if ( typeof message === 'object')
-        message = message.toString();
+	var text, variable;
+	var tokens = [];
+	var result = '';
 
-    var tokens = message.split(VALUE_REGEXP);
-    var result = '';
+	if ( typeof message !== 'object')
+		tokens = message.split(VALUE_REGEXP);
+	else
+		tokens = message;
 
-    switch (style) {
-        case "info":
-            text     = chalk.cyan;
-            variable = chalk.cyan.bold;
-            break;
-        case "success":
-            text     = chalk.green;
-            variable = chalk.green.bold;
-            break;
-        case "warning":
-            text     = chalk.yellow;
-            variable = chalk.yellow.bold;
-            break;
-        case "error":
-            text     = chalk.red;
-            variable = chalk.red.bold;
-            break;
-        case "note":
-            text     = chalk.gray;
-            variable = chalk.white;
-            break;
-        case "time":
-            text     = chalk.magenta;
-            variable = chalk.magenta.bold;
-            break;
-        case "debug":
-            text     = chalk.grey.dim;
-            variable = chalk.grey.dim.bold;
-            break;
-        default:
-            text     = chalk.gray;
-            variable = chalk.white;
-            break;
-    }
+	switch (style) {
+		case "info":
+			text     = chalk.cyan;
+			variable = chalk.cyan.bold;
+			break;
+		case "success":
+			text     = chalk.green;
+			variable = chalk.green.bold;
+			break;
+		case "warning":
+			text     = chalk.yellow;
+			variable = chalk.yellow.bold;
+			break;
+		case "error":
+			text     = chalk.red;
+			variable = chalk.red.bold;
+			break;
+		case "note":
+			text     = chalk.gray;
+			variable = chalk.white;
+			break;
+		case "time":
+			text     = chalk.magenta;
+			variable = chalk.magenta.bold;
+			break;
+		case "debug":
+			text     = chalk.grey.dim;
+			variable = chalk.grey.dim.bold;
+			break;
+		default:
+			text     = chalk.gray;
+			variable = chalk.white;
+			break;
+	}
 
-    // if we dont have bold variables (for merging), set variable to text color
-    if( ! defOptions.boldVariables ) {
-        variable = text;
-        if ( text === chalk.gray ) {
-            variable = chalk.white;
-        }
-    }
+	// if we dont have bold variables (for merging), set variable to text color
+	if( ! defOptions.boldVariables ) {
+		variable = text;
+		if ( text === chalk.gray ) {
+			variable = chalk.white;
+		}
+	}
 
-    // interpolate string
-    for (var i = 0; i < tokens.length; i++) {
-        if (i%2) {
-            result += variable(_.deepGet(data, tokens[i]) || '');
-        } else {
-            result += text(tokens[i] || '');
-        }
-    }
+	// interpolate string
+	if ( typeof tokens == 'object') {
+		result = text(tokens);
+	} else {
+		for (var i = 0; i < tokens.length; i++) {
+			if (i%2) {
+				result += variable(_.deepGet(data, tokens[i]) || '');
+			} else {
+				result += text(tokens[i] || '');
+			}
+		}
+	}
 
-    function setLine(line) {
-        if (!line) { return; }
+	function setLine(line) {
+		if (!line) { return; }
 
-        var result = '';
+		var result = '';
 
-        for (var i = 0; i < 80; i++) {
-            result += line;
-        }
+		for (var i = 0; i < 80; i++) {
+			result += line;
+		}
 
-        if ( defOptions.logToConsole ) { console.log(text(result)); }
-    }
+		if ( defOptions.logToConsole ) { console.log(text(result)); }
+	}
 
-    function logToFile(style, result) {
-        switch (style) {
-            case 'error':
-              if (defOptions.logToFile) { logger.error(result); }
-              break;
-           case 'warning':
-              if (defOptions.logToFile) { logger.warn(result); }
-              break;
-           case 'info':
-              if (defOptions.logToFile) { logger.info(result); }
-              break;
-           case 'success':
-              if (defOptions.logToFile) { logger.info(result); }
-              break;
-           case 'time':
-              if (defOptions.logToFile) { logger.info(result); }
-              break;
-          case 'debug':
-              if (defOptions.logToFile) { logger.log('debug', result); }
-              break;
-          case 'default':
-              if (defOptions.logToFile) { logger.info(result); }
-              break;
-        }
-    }
+	function logToFile(style, result) {
+		switch (style) {
+			case 'error':
+				if (defOptions.logToFile) { logger.error(result); }
+				break;
+			case 'warning':
+				if (defOptions.logToFile) { logger.warn(result); }
+				break;
+			case 'info':
+				if (defOptions.logToFile) { logger.info(result); }
+				break;
+			case 'success':
+				if (defOptions.logToFile) { logger.info(result); }
+				break;
+			case 'time':
+				if (defOptions.logToFile) { logger.info(result); }
+				break;
+			case 'debug':
+				if (defOptions.logToFile) { logger.log('debug', result); }
+				break;
+			case 'default':
+				if (defOptions.logToFile) { logger.info(result); }
+				break;
+		}
+	}
 
-    setLine(before);
-    if ( defOptions.logToConsole ) { console.log(result); }
-
-    setLine(after);
-    logToFile(style, result);
+	setLine(before);
+	if ( defOptions.logToConsole ) { console.log(result); }
+	setLine(after);
+	logToFile(style, result);
 
 }
 
 function getArgs(args) {
-    var result = {
-        before:  args[0],
-        message: args[1],
-        after:   args[2],
-        data:    args[3]
-    };
+	var result = {
+		before:  args[0],
+		message: args[1],
+		after:   args[2],
+		data:    args[3]
+	};
 
-    if (typeof args[1] !== 'string') {
-        result.before  = null;
-        result.message = args[0];
-        result.after   = null;
-        result.data    = args[1];
+	if (typeof args[1] !== 'string') {
+		result.before  = null;
+		result.message = args[0];
+		result.after   = null;
+		result.data    = args[1];
 
-    } else if (typeof args[2] !== 'string') {
-        result.before  = args[0];
-        result.message = args[1];
-        result.after   = null;
-        result.data    = args[2];
-    }
+	} else if (typeof args[2] !== 'string') {
+		result.before  = args[0];
+		result.message = args[1];
+		result.after   = null;
+		result.data    = args[2];
+	}
 
-    result.data = _.merge({env: process.env}, result.data);
+	result.data = _.merge({env: process.env}, result.data);
 
-    return result;
+	return result;
 }
 
 function msg(style, useFlush) {
-    var totalStart    = process.hrtime();
+	var totalStart    = process.hrtime();
 
-    return function() {
-        var args     = getArgs(arguments);
-        var lastFile = {};
-        var start    = process.hrtime();
+	return function() {
+		var args     = getArgs(arguments);
+		var lastFile = {};
+		var start    = process.hrtime();
 
-        function transform(file, callback) {
-            lastFile = file;
+		function transform(file, callback) {
+			lastFile = file;
 
-            if (!useFlush) {
-                args.data.file          = _.clone(file);
-                args.data.file.relative = path.relative(file.base, file.path);
-                args.data.file.basename = path.basename(file.path);
-                args.data.duration      = prettyHrtime(process.hrtime(start));
-                args.data.totalDuration = prettyHrtime(process.hrtime(totalStart));
+			if (!useFlush) {
+				args.data.file          = _.clone(file);
+				args.data.file.relative = path.relative(file.base, file.path);
+				args.data.file.basename = path.basename(file.path);
+				args.data.duration      = prettyHrtime(process.hrtime(start));
+				args.data.totalDuration = prettyHrtime(process.hrtime(totalStart));
 
-                notify(style, args.before, args.message, args.after, args.data);
-            }
-            callback(null, file);
-        }
+				notify(style, args.before, args.message, args.after, args.data);
+			}
+			callback(null, file);
+		}
 
-        function flush(callback) {
-            if (useFlush) {
-                args.data.file          = _.clone(lastFile);
-                args.data.duration      = prettyHrtime(process.hrtime(start));
-                args.data.totalDuration = prettyHrtime(process.hrtime(totalStart));
+		function flush(callback) {
+			if (useFlush) {
+				args.data.file          = _.clone(lastFile);
+				args.data.duration      = prettyHrtime(process.hrtime(start));
+				args.data.totalDuration = prettyHrtime(process.hrtime(totalStart));
 
-                notify(style, args.before, args.message, args.after, args.data);
-            }
-            callback();
-        }
+				notify(style, args.before, args.message, args.after, args.data);
+			}
+			callback();
+		}
 
-        return through.obj(transform, flush);
-    };
+		return through.obj(transform, flush);
+	};
 }
 
 function init() {
 
-    return function(options) {
+	return function(options) {
 
-        if ( typeof options !== 'undefined') {
-            defOptions = defaults(options, defOptions);
-        }
+		if ( typeof options !== 'undefined') {
+			defOptions = defaults(options, defOptions);
+		}
 
-        if(defOptions.logPath[defOptions.logPath.length] !== '/') {
-            defOptions.logPath += '/';
-        }
+		if(defOptions.logPath[defOptions.logPath.length] !== '/') {
+			defOptions.logPath += '/';
+		}
 
-        defOptions.logFilename = defOptions.logPath + defOptions.logFile;
-        if ( defOptions.rotateLog ) {
-            logger.add(winston.transports.DailyRotateFile,{filename: defOptions.logFilename});
-        } else {
-            logger.add(winston.transports.File,{filename: defOptions.logFilename});
-        }
+		defOptions.logFilename = defOptions.logPath + defOptions.logFile;
+		if ( defOptions.rotateLog ) {
+			logger.add(winston.transports.DailyRotateFile,{filename: defOptions.logFilename});
+		} else {
+			logger.add(winston.transports.File,{filename: defOptions.logFilename});
+		}
 
-        if(defOptions.logToFile) {
-            mkdirp(defOptions.logPath);
-        }
-        defOptions.logInitialized = true;
-    };
+		if(defOptions.logToFile) {
+			mkdirp(defOptions.logPath);
+		}
+		defOptions.logInitialized = true;
+	};
 }
 
 function Msg(style) {
-    return function() {
-        var args = getArgs(arguments);
-        notify(style, args.before, args.message, args.after, args.data);
-    };
+	return function() {
+		var args = getArgs(arguments);
+		notify(style, args.before, args.message, args.after, args.data);
+	};
 }
 
 
 module.exports = {
-    init:    init(),
-    info:    msg('info'),
-    success: msg('success'),
-    warning: msg('warning'),
-    warn:    msg('warning'),
-    error:   msg('error'),
-    note:    msg('note'),
-    time:    msg('time'),
-    debug:   msg('debug'),
-    flush: {
-        info:    msg('info', true),
-        success: msg('success', true),
-        warning: msg('warning', true),
-        warn:    msg('warning', true),
-        error:   msg('error', true),
-        note:    msg('note', true),
-        time:    msg('time', true),
-        debug:   msg('debug', true)
-    },
-    Info:    new Msg('info'),
-    Success: new Msg('success'),
-    Warning: new Msg('warning'),
-    Warn:    new Msg('warning'),
-    Error:   new Msg('error'),
-    Note:    new Msg('note'),
-    Time:    new Msg('time'),
-    Debug:   new Msg('debug')
+	init:    init(),
+	info:    msg('info'),
+	success: msg('success'),
+	warning: msg('warning'),
+	warn:    msg('warning'),
+	error:   msg('error'),
+	note:    msg('note'),
+	time:    msg('time'),
+	debug:   msg('debug'),
+	flush: {
+		info:    msg('info', true),
+		success: msg('success', true),
+		warning: msg('warning', true),
+		warn:    msg('warning', true),
+		error:   msg('error', true),
+		note:    msg('note', true),
+		time:    msg('time', true),
+		debug:   msg('debug', true)
+	},
+	Info:    new Msg('info'),
+	Success: new Msg('success'),
+	Warning: new Msg('warning'),
+	Warn:    new Msg('warning'),
+	Error:   new Msg('error'),
+	Note:    new Msg('note'),
+	Time:    new Msg('time'),
+	Debug:   new Msg('debug')
 };
